ba4ee8ae7b1c2dff2a1bd465463df8969e6c01fc
Priyansh Malik <priyanshmalik332@gmail.com>
2024-07-04 02:03:44 +0530

    "Again Added Url"

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,7 @@ mongoose.connect(process.env.MONGODB_URI).then(()=>{
 
 const app = express();
 
-app.use(cors({
-    origin: ["https://e-commerce-project-4hgd.vercel.app"],
-    method: ["POST", "GET"],
-    credentials: true
-}))
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
